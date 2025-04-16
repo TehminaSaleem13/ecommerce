ea69c9ae5955b1874abf6f82351ece804186fa44
Tehmina Saleem <tehminasaleem13@gmail.com>
2025-04-16 13:26:06 +0500

    add to cart functionality for seller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,11 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
     if user_signed_in?
       if current_user.role == 'seller'
-        @products = current_user.products.page(params[:page]).per(3)
+        @your_products = current_user.products.page(params[:your_products_page]).per(3)
+        @all_products = Product.where.not(user: current_user).page(params[:all_products_page]).per(3)
       else
         @products = Product.page(params[:page]).per(3)
       end
